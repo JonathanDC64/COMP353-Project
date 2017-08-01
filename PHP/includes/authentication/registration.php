f5351b32267a3791794383fdf2bf41a5d00e7fe0
JonathanDC64 <nintendo_jon64@hotmail.com>
2017-08-01 15:04:46 -0400

    Working on auth

---
+++ PHP/includes/authentication/registration.php
@@ -0,0 +1,12 @@
+<?php
+    // User
+    $Username = $_POST["Username"];
+    $Password = $_POST["Password"];
+    # Access rights
+
+    // UserInformation
+    $UserID = null;
+    $First_Name = $_POST["First_Name"];
+    $Last_Name = $_POST["Last_Name"];
+    $Phone_Number = $_POST["Phone_Number"];
+?>
