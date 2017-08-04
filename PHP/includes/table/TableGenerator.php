b963813956b570f5dfb408b06fe5fa9c723b4ef7
JonathanDC64 <nintendo_jon64@hotmail.com>
2017-08-04 14:49:02 -0400

    Made progress on table generator

--- PHP/includes/table/TableGenerator.php
+++ PHP/includes/table/TableGenerator.php
@@ -13,7 +13,7 @@
     }
 
     class TableGenerator{
-        public static function generate_table($Title, $ColumnNames, $RowData, $UpdateURL, $DeleteURL, $Permissions){
+        public static function generate_table($Title, $ColumnNames, $RowData, $UpdateURL, $DeleteURL, $Permissions, $UpdateFormURL){
             $User = User::get_user_info();
             echo "
                 <table class='table'>
@@ -32,10 +32,27 @@
                     </thead>
                     <tbody>";
                         foreach($RowData as $Row){
-                            TableGenerator::display_row($Row, $User->AccessLevel, $Permissions);
+                            TableGenerator::display_row($Row, $User->AccessLevel, $Permissions, $Title);
                         }
             echo "  </tbody>
                 </table>
+                <div id='$Title-Modal' class='modal fade' role='dialog'>
+                    <div class='modal-dialog'>
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                                <h4 class='modal-title'>Update $Title</h4>
+                            </div>
+                            <div class='modal-body'>";
+                                if(isset($UpdateFormURL) && $UpdateFormURL !== "")
+                                    include($UpdateFormURL);
+            echo "          </div>
+                            <div class='modal-footer'>
+                                <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             ";
         }
 
@@ -43,7 +60,7 @@
             echo "<th>$ColumnName</th>";
         }
 
-        private static function display_row($Row, $AccessLevel, $Permissions){
+        private static function display_row($Row, $AccessLevel, $Permissions, $Title){
             echo "<tr>";
                 foreach($Row as $Cell){
                     echo "<td>";
@@ -51,7 +68,7 @@
                     echo "</td>";
                 }
                 if($AccessLevel >= $Permissions->UpdateAccessLevel){
-                    echo "<td><button type='button' class='btn btn-primary'><span class='glyphicon glyphicon-pencil'></span></button></td>";
+                    echo "<td><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#$Title-Modal'><span class='glyphicon glyphicon-pencil'></span></button></td>";
                 }
                 if($AccessLevel >= $Permissions->DeleteAccessLevel){
                     echo "<td><button type='button' class='btn btn-danger'><span class='glyphicon glyphicon-trash'></span></button></td>";
