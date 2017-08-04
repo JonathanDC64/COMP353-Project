e40b7021bf62cf526c3194dbc122fa9aaf12924a
Ali At-Tayeb <30572881+attayeba@users.noreply.github.com>
2017-08-04 12:35:35 -0400

    Created Equipment Page

---
+++ PHP/includes/Equipment/Equipment.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Class to add equipment
+    class Equipment{
+		
+        // Creates an equipment
+        public static function create_equipment($Name){
+            global $connection;
+            $stmt = $connection->prepare("INSERT INTO Equipment VALUES(0,:Name)"); 
+            $stmt->bindParam(':Name', $Name);
+            $stmt->execute();
+            return $connection->lastInsertId();
+        }
+	
+        // Check  if equipment already exists
+        public static function equipment_exists($Name){
+            global $connection;
+			$stmt = $connection->prepare("SELECT Name FROM Equipment WHERE Name = :Name"); 
+            $stmt->bindParam(':Name', $Name);
+            $stmt->execute();
+            $row = $stmt->fetch();
+            return $stmt->rowCount() > 0;
+        }
+	
+    }
+?>
