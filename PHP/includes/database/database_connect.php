6029693ec10145e77de1e891d5c9b81bd9bd0ab9
JonathanDC64 <nintendo_jon64@hotmail.com>
2017-08-08 17:03:06 -0400

    dynamic database connection

--- PHP/includes/database/database_connect.php
+++ PHP/includes/database/database_connect.php
@@ -1,11 +1,25 @@
 <?php
     //echo shell_exec("ssh -L 3306:ytc353_1.encs.concordia.ca:3306 login.encs.concordia.ca");
     // Database info
-    $servername = "localhost";//"ytc353_1.encs.concordia.ca";
-    $dbName = "ytc353_1";
-    $username = "root";//"ytc353_1";
-    $password = "";//"jdcasyra";
+    $servername = null;
+    $dbName = null;
+    $username = null;
+    $password = null;
     $connection = null;
+    if(strpos($_SERVER["HTTP_HOST"], "ytc353_1")){
+        $servername = "ytc353_1.encs.concordia.ca";
+        $dbName = "ytc353_1";
+        $username = "ytc353_1";
+        $password = "jdcasyra";
+        $connection = null;
+    }
+    else{
+        $servername = "localhost";//"ytc353_1.encs.concordia.ca";
+        $dbName = "ytc353_1";
+        $username = "root";//"ytc353_1";
+        $password = "";//"jdcasyra";
+        $connection = null;
+    }
 
     // Attempt to connect
     try {
