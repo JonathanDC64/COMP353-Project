d34f017b6b854e352042adf2d23ba4998482fca0
Ali At-Tayeb <30572881+attayeba@users.noreply.github.com>
2017-08-08 16:25:06 -0400

    fixed the forms for therapists

--- PHP/webroot/my_appointment.php
+++ PHP/webroot/my_appointment.php
@@ -20,12 +20,6 @@
                     $Appointment = Appointment::get_patient_doctor_appointment($AppointmentID)[0];
                     $Prescription = Appointment::get_doctor_prescription($AppointmentID);
                     $Title = "Doctors Appointment";
-                }
-                else{ //Therapist
-                    $Appointment = Appointment::get_patient_therapist_appointment($AppointmentID)[0];
-                    $Prescription = Appointment::get_therapist_prescription($AppointmentID);
-                    $Title = "Therapist Appointment";
-                }
 ?>
                 <div id="content">
                     <h2><?= $Title; ?></h2>
@@ -50,9 +44,60 @@
                             <td><strong>Diagnosis:</strong></td>
                             <td><?= isset($Prescription[0]["Diagnosis"]) ? $Prescription[0]["Diagnosis"] : ""; ?></td>
                         </tr>
+						
+						
                     </table>
                 </div>
 <?php
+                }
+                else{ //Therapist
+                    $Appointment = Appointment::get_patient_therapist_appointment($AppointmentID)[0];
+                    $Prescription = Appointment::get_therapist_prescription($AppointmentID);
+                    $Title = "Therapist Appointment";
+					
+?>
+				 <div id="content">
+                    <h2><?= $Title; ?></h2>
+                    <table class="table well">
+                        <tr>
+                            <td><strong>Appointment ID:</strong></td>
+                            <td><?= $Appointment["AppointmentID"];  ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Appointment Date:</strong></td>
+                            <td><?= $Appointment["Appointment_Date"] ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Doctor Name:</strong></td>
+                            <td><?= $Appointment["First_Name"] . " " . $Appointment["Last_Name"]; ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Doctors Note:</strong></td>
+                            <td><?= isset($Prescription[0]["DoctorsNote"]) ? $Prescription[0]["DoctorsNote"] : ""; ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Diagnosis:</strong></td>
+                            <td><?= isset($Prescription[0]["Diagnosis"]) ? $Prescription[0]["Diagnosis"] : ""; ?></td>
+                        </tr>
+						<tr>
+                            <td><strong>Treatment:</strong></td>
+                            <td><?= isset($Prescription[0]["Description"]) ? $Prescription[0]["Description"] : ""; ?></td>
+                        </tr>
+						<tr>
+                            <td><strong>Treatment Cost:</strong></td>
+                            <td><?= isset($Prescription[0]["Cost"]) ? $Prescription[0]["Cost"] : ""; ?></td>
+                        </tr>
+						<tr>
+                            <td><strong>Equipment:</strong></td>
+                            <td><?= isset($Prescription[0]["Name"]) ? $Prescription[0]["Name"] : ""; ?></td>
+                        </tr>
+						
+						
+                    </table>
+                </div>
+				
+<?php				
+			   }
             }
             else{
                 echo "This is not your appointment.";
@@ -83,6 +128,10 @@
 
             if($validated){
                 $Prescription = count($Prescription) > 0 ? $Prescription[0] : null;
+				
+				if($User->Role == "Doctor")
+				{
+					
 ?>
                 <div id="content">
                     <h2>My <?= $User->Role; ?> Appointment</h2>
@@ -111,6 +160,51 @@
                     </table>
                 </div>
 <?php
+				}
+				else
+				{
+					
+?>
+                <div id="content">
+                    <h2>My <?= $User->Role; ?> Appointment</h2>
+                    <a href="<?= $UpdateButton; ?>.php?AppointmentID=<?= $Appointment["AppointmentID"]; ?>&Type=<?= $User->Role; ?>" class='btn btn-info' role='button'>Update Record</a>						
+                    <table class="table well">
+                        <tr>
+                            <td><strong>Appointment ID:</strong></td>
+                            <td><?= $Appointment["AppointmentID"];  ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Appointment Date:</strong></td>
+                            <td><?= $Appointment["Appointment_Date"] ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Patient Name:</strong></td>
+                            <td><?= $Appointment["First_Name"] . " " . $Appointment["Last_Name"]; ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Doctors Note:</strong></td>
+                            <td><?= isset($Prescription["DoctorsNote"]) ? $Prescription["DoctorsNote"] : ""; ?></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Diagnosis:</strong></td>
+                            <td><?= isset($Prescription["Diagnosis"]) ? $Prescription["Diagnosis"] : ""; ?></td>
+                        </tr>
+						<tr>
+                            <td><strong>Treatment:</strong></td>
+                            <td><?= isset($Prescription["Description"]) ? $Prescription["Description"] : ""; ?></td>
+                        </tr>
+						<tr>
+                            <td><strong>Cost:</strong></td>
+                            <td><?= isset($Prescription["Cost"]) ? $Prescription["Cost"] : ""; ?></td>
+                        </tr>
+						<tr>
+                            <td><strong>Equipment:</strong></td>
+                            <td><?= isset($Prescription["Name"]) ? $Prescription["Name"] : ""; ?></td>
+                        </tr>
+                    </table>
+                </div>
+<?php
+				}
             }
             else{
                 echo "This is not your appointment.";
